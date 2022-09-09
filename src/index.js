f8a61a015c5b153a308de40298e49cb4100c6512
Weslley <weslley@pop-os.localdomain>
2022-09-09 17:10:26 -0300

    feat(apostila05): fim da apostila

--- src/index.js
+++ src/index.js
@@ -18,12 +18,13 @@ class App extends React.Component{
 
     constructor(props){
         super(props)
-            this.state={
+        this.state={
                 latitude: null,
                 longitude: null,
                 estacao:null,
                 data: null,
-                icone: null
+                icone: null,
+                mensagemDeErro: null
             }
         }
 
@@ -45,12 +46,73 @@ obterEstacao = (data, latitude)=>{
 }
     render(){
         return(
-        <div>
-            Meu App
+        <div className="container mt-2">
+            <div className="row justify-content-center">
+                <div className="col-mt-8">
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="d-flex align-items-center border rounded mb-2" style={{height: '6rem'}}>
+                                <i className={`fas fa-5x ${this.state.icone}`}></i>
+                                <p className="w-75 ms-3 text-center fs-1">
+                                    {this.state.estacao}
+                                </p>
+                            </div>
+                            <div>
+                                <p className="text-center">
+                                    {
+                                        //expressaoLogica ? v1 : v2
+                                        this.setState.latitude ?
+                                            `Coordenadas: ${this.state.latitude}, ${this.state.longitude}. Data: ${this.state.data}`
+                                        : this.state.mensagemDeErro ? 
+                                        `${this.state.mensagemDeErro}`
+                                        :
+                                            `Clique no botão para saber a sua estação climática`
+                                    }
+                                </p>
+                            </div>
+                            <button className='btn btn-outline-primary w-100 mt-2'onClick={this.obterLocalizacao}>
+                                Qual é a minha estação?
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         )
     }
+
+    icones = {
+        'Primavera': 'fa-seedling',
+        'Verão': 'fa-umbrella-beach',
+        'Outono': 'fa-tree',
+        'Inverno': 'fa-snowman'
+        };
+    
+    
+    obterLocalizacao = ()=>{
+        window.navigator.geolocation.getCurrentPosition(
+            //função callback
+            (posicao) =>{
+                let data = new Date()
+                let estacao = this.obterEstacao(data, posicao.coords.latitude)
+                let icone = this.icones[estacao]
+                console.log(icone)
+                this.setState({
+                    latitude: posicao.coords.latitude,
+                    longitude: posicao.coords.longitude,
+                    estacao: estacao,
+                    data: data.toLocaleString(),
+                    icone: icone
+                })
+            },
+            (err)=>{
+                console.log(err)
+                this.setState({mensagemDeErro: 'Tente novamente mais tarde'})
+            }
+        )
+    }; 
 }
+
 
 ReactDOM.render(
     <App/>,
